a4acc81c499cd829fe84244bfa07da66a8b54917
Emir Takayama <ngemir@gmail.com>
2023-02-16 12:45:34 -0300

    Corrigindo a Validação do formulário

--- src/components/JanelaDeCadastro/index.jsx
+++ src/components/JanelaDeCadastro/index.jsx
@@ -1,11 +1,11 @@
-import React, {useState, useEffect} from 'react';
+import React, { useState, useEffect } from 'react';
 import './index.css';
 
 
 
 //Renderização
 export default function JanelaDeCadastro() {
-  
+
   // Criação de Estados
   const [ingressante, setIngressante] = useState({
     nome: "",
@@ -14,46 +14,53 @@ export default function JanelaDeCadastro() {
     cidade: ""
   });
 
+  const [erro, setErro] = useState({
+    nome: "",
+    curso: "",
+    estado: "",
+    cidade: ""
+  })
+
   //ação do botão Voltar
-  function limparCampos(){
+  function limparCampos() {
     setIngressante({
       nome: "",
       curso: "",
       estado: "",
       cidade: ""
     })
-    
+
     setEstadoNaoSelecionado(true);
   }
 
   //Funções para lidar com os dados dos setStates do dado de Registro
-  function lidarInputNome(nome){
+  function lidarInputNome(nome) {
     let valorNovo = {};
-    valorNovo = {nome: nome.target.value};
+    valorNovo = { nome: nome.target.value };
     setIngressante(ingressante => ({
       ...ingressante,
       ...valorNovo
     }));
   }
-  
-  function lidarInputCurso(curso){
-    setIngressante(ingressante => ({...ingressante, curso: curso.target.value}));
+
+  function lidarInputCurso(curso) {
+    setIngressante(ingressante => ({ ...ingressante, curso: curso.target.value }));
   }
-  
-  function lidarInputEstado(estado){
-    setIngressante(ingressante => ({...ingressante, estado: estado.target.value}));
-    
+
+  function lidarInputEstado(estado) {
+    setIngressante(ingressante => ({ ...ingressante, estado: estado.target.value }));
+
     //bloqueia o menu dropdows da cidade quando não tem Estado selecionado
-    if(estado.target.value === ""){
+    if (estado.target.value === "") {
       setEstadoNaoSelecionado(true);
       ingressante.cidade = ''
-    }else{
+    } else {
       setEstadoNaoSelecionado(false);
     }
   }
 
-  function lidarInputCidade(cidade){
-    setIngressante(ingressante => ({...ingressante, cidade: cidade.target.value}));
+  function lidarInputCidade(cidade) {
+    setIngressante(ingressante => ({ ...ingressante, cidade: cidade.target.value }));
   }
 
   // fim das funções lidar
@@ -63,26 +70,26 @@ export default function JanelaDeCadastro() {
   //Tratamento dos Cursos existente no JSON
   const [cursos, setCursos] = useState([]);
 
-  const listarCursos=()=>{
+  const listarCursos = () => {
     fetch('./data/cursos.json'
-    ,{
-      headers : { 
-        'Content-Type': 'application/json',
-        'Accept': 'application/json'
-       }
-    }
+      , {
+        headers: {
+          'Content-Type': 'application/json',
+          'Accept': 'application/json'
+        }
+      }
     )
-      .then(function(response){
+      .then(function (response) {
         return response.json();
       })
-      .then(function(dados) {
+      .then(function (dados) {
         setCursos(dados.Cursos);
       });
   }
-  useEffect(()=>{
+  useEffect(() => {
     listarCursos()
-  },[])
-  
+  }, [])
+
 
   //Fim do tratamento da lista Curso
 
@@ -90,73 +97,88 @@ export default function JanelaDeCadastro() {
   const [estadoNaoSelecionado, setEstadoNaoSelecionado] = useState(true);
   const [estados, setEstados] = useState([]);
 
-  const listarEstados=()=>{
+  const listarEstados = () => {
     fetch('./data/estadoecidade.json'
-    ,{
-      headers : { 
-        'Content-Type': 'application/json',
-        'Accept': 'application/json'
-       }
-    }
+      , {
+        headers: {
+          'Content-Type': 'application/json',
+          'Accept': 'application/json'
+        }
+      }
     )
-      .then(function(response){
+      .then(function (response) {
         return response.json();
       })
-      .then(function(dados) {
+      .then(function (dados) {
         let es = Object.keys(dados.Estados)
         setEstados(es);
       });
   }
-  useEffect(()=>{
+  useEffect(() => {
     listarEstados()
-  },[])
-  
+  }, [])
+
 
   //Tratamento da lista de cidades de acordo com o Estado selecionado
   const [cidades, setCidades] = useState([]);
-  
-  const listarCidades=()=>{
+
+  const listarCidades = () => {
     fetch('./data/estadoecidade.json'
-    ,{
-      headers : { 
-        'Content-Type': 'application/json',
-        'Accept': 'application/json'
-       }
-    }
+      , {
+        headers: {
+          'Content-Type': 'application/json',
+          'Accept': 'application/json'
+        }
+      }
     )
-      .then(function(response){
+      .then(function (response) {
         return response.json();
       })
-      .then(function(dados) {
+      .then(function (dados) {
         const estadoSelecionado = ingressante.estado
         const ci = dados.Estados[estadoSelecionado]
         setCidades(ci.cidades);
       });
   }
-  useEffect(()=>{
+  useEffect(() => {
     listarCidades()
-  },[ingressante])
+  }, [ingressante])
 
   // Fim do tratamento da Lista de Estado
-  
+
   //Fim da listagem ↑ 
 
   //Enviar Registro para alert
-  function enviar(event){
+  function enviar(event) {
+
     event.preventDefault();
     let nomeInserido = event.target[0].value;
     let cursoSelecionado = event.target[1].value;
     let estadoSelecionado = event.target[2].value;
     let cidadeSelecionado = event.target[3].value;
-    
-    let dadoEnviado = {
-      nome: nomeInserido,
-      curso: cursoSelecionado,
-      estado: estadoSelecionado,
-      cidade: cidadeSelecionado
+
+    //tratamento de nome incorreto
+    let teveErro = false;
+
+    if (nomeInserido.match(/[^A-Za-z ]+/) || nomeInserido === null) {
+      let erroMSG = 'Não pode conter número ou caracteres especiais.';
+      setErro({nome: erroMSG});
+      teveErro = true;
     }
 
-    alert(JSON.stringify(dadoEnviado));
+    console.log(erro);
+
+    if(!teveErro){
+      setErro({nome: ''})
+      let dadoEnviado = {
+        nome: nomeInserido,
+        curso: cursoSelecionado,
+        estado: estadoSelecionado,
+        cidade: cidadeSelecionado
+      }
+
+      alert(JSON.stringify(dadoEnviado));
+    }
   }
 
   //Interface
@@ -168,12 +190,13 @@ export default function JanelaDeCadastro() {
       <form className='formulario' onSubmit={enviar}>
         <div className='campoDePreenchimento'>
           <label htmlFor="nome">Nome</label>
-          <input type="text" value={ingressante.nome} name="inputNome" id="nome" onChange={(nomeInserido) => lidarInputNome(nomeInserido)}/>
+          <input type="text" required value={ingressante.nome} name="inputNome" id="nome" onChange={(nomeInserido) => lidarInputNome(nomeInserido)} />
+          {erro.nome ? <span className='msgErro'>{erro.nome}</span> : null}
         </div>
         <div className='campoDePreenchimento'>
           <label htmlFor="curso">Curso</label>
           <div className="seta">
-            <select name="inputCurso" value={ingressante.curso} id="curso" onChange={(cursoEscolhido) => lidarInputCurso(cursoEscolhido)}>
+            <select name="inputCurso" required value={ingressante.curso} id="curso" onChange={(cursoEscolhido) => lidarInputCurso(cursoEscolhido)}>
               <option value="" defaultChecked>Escolha seu Curso</option>
               {cursos.map((item) => <option key={item} value={item}>{item}</option>)}
             </select>
@@ -181,17 +204,17 @@ export default function JanelaDeCadastro() {
         </div>
         <div className='campoDePreenchimento'>
           <label htmlFor="estado">Estado</label>
-          <div className="seta">  
-            <select name="inputEstado" value={ingressante.estado} id="estado" onChange={(estadoEscolhido) => lidarInputEstado(estadoEscolhido)}>
+          <div className="seta">
+            <select name="inputEstado" required value={ingressante.estado} id="estado" onChange={(estadoEscolhido) => lidarInputEstado(estadoEscolhido)}>
               <option value="" defaultChecked>Escolha seu Estado</option>
-              {estados.map(estado => <option key={estado} value={estado}>{estado}</option> )}
+              {estados.map(estado => <option key={estado} value={estado}>{estado}</option>)}
             </select>
           </div>
         </div>
         <div className='campoDePreenchimento'>
           <label htmlFor="cidade">Cidades</label>
           <div className="seta">
-            <select name="inputCidade" value={ingressante.cidade} id="cidade" disabled={estadoNaoSelecionado} onChange={(cidadeEscolhido) => lidarInputCidade(cidadeEscolhido)}>
+            <select name="inputCidade" required value={ingressante.cidade} id="cidade" disabled={estadoNaoSelecionado} onChange={(cidadeEscolhido) => lidarInputCidade(cidadeEscolhido)}>
               <option value="" defaultChecked>Escolha sua Cidade</option>
               {cidades.map(cidade => <option key={cidade} value={cidade}>{cidade}</option>)}
             </select>
